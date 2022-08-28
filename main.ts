349748e4e5a0345b682bf6c713e3d04b95bf137f
bumpy09 <101124139+bumpy09@users.noreply.github.com>
2022-08-28 15:09:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+edubitSoundBit.onEvent(SoundSensorCompareType.MoreThan, 10, function () {
+    edubitMotors.runMotor(MotorChannel.M1, MotorDirection.Forward, 255)
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    basic.showIcon(IconNames.No)
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    basic.showIcon(IconNames.No)
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+})
+basic.forever(function () {
+    if (edubitIrBit.isIrSensorTriggered()) {
+        edubitMotors.brakeMotor(MotorChannel.M1)
+    }
+})
