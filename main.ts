3ae5f31cb9df2ee72a72b44dc95740431fb8c618
bumpy09 <101124139+bumpy09@users.noreply.github.com>
2022-08-28 16:31:12 +0800

    MAKE It better

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+})
 edubitSoundBit.onEvent(SoundSensorCompareType.MoreThan, 10, function () {
     edubitMotors.runMotor(MotorChannel.M1, MotorDirection.Forward, 255)
     edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
